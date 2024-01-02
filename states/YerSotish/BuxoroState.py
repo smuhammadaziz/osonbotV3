353096197e9d4ec89b5735c8a7aad00c65ecf469
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-01-02 21:00:01 +0500

    add: adding some states

---
+++ states/YerSotish/BuxoroState.py
@@ -0,0 +1,19 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class BuxoroYerSotish(StatesGroup):
+    images = State()
+    umumiyMaydon = State()
+    gaz = State()
+    svet = State()
+    suv = State()
+    kanal = State()
+    qoshimchaMalumot = State()
+    hujjatlar = State()
+    valyuta = State()
+    narxi = State()
+    manzil = State()
+    moljal = State()
+    telNumberOne = State()
+    telNumberTwo = State()
+    check = State()
